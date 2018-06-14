f1511ffd6da25b4a44a4ad779e2011b0fe996505
WalterDuartesUB <37937023+WalterDuartesUB@users.noreply.github.com>
2018-06-13 22:19:24 -0300

    Se implementa la seleccion de mapas de una lista por la consola al usuario

--- src/ar/edu/ub/buscaminas/menu/MenuMapas.java
+++ src/ar/edu/ub/buscaminas/menu/MenuMapas.java
@@ -1,19 +1,40 @@
 package ar.edu.ub.buscaminas.menu;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import ar.edu.ub.buscaminas.Consola;
 
 public class MenuMapas {
-	public static String obtenerPathMapa( Consola consola, String pathMapa ) {
-		File directorio = new File( pathMapa );
+	public static String obtenerPathMapa( Consola consola, String directorioDeMapas ) {
+		File directorio = new File( directorioDeMapas );
+		Map<String, String> nombresMapas = new HashMap<String, String>();
+		String       pathMapaUsuario = null;
+		int          posicion = 1;
 		
-		consola.println("Elegi el mapa en el que queres jugar: ");
+		//Levanto los archivos del directorio
+	    for (File file : directorio.listFiles()) {
+	        if ( !file.isDirectory()) {
+	        	nombresMapas.put( String.format("%d", posicion), file.getName());
+	        	posicion++;
+	        }
+	            	    	
+	    }
+	    
+	    //Pido al usuario que elija uno de la lista
+		while( pathMapaUsuario == null ) {
+			
+			consola.println("Elegi el mapa en el que queres jugar: ");
+			
+			for( String opcion : nombresMapas.keySet() )
+				consola.println( opcion + " - " + nombresMapas.get(opcion));
+			
+			pathMapaUsuario = nombresMapas.get( consola.nextLine() );
+		}
 		
-	    for (File file : directorio.listFiles())	    	
-	        if ( !file.isDirectory()) 
-	            consola.println(file.getName());	        	   
-		
-	    return consola.nextLine();
+	    return directorioDeMapas + pathMapaUsuario;
 	}
 }
