c5f4a44e6e40d281905727671dc369d3392311cd
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-06-14 16:54:41 -0300

    Se empieza el testcase de CasilllaBlanco

--- testcase/ar/edu/ub/buscaminas/casilla/testcase/TestCasillaBlanco.java
+++ testcase/ar/edu/ub/buscaminas/casilla/testcase/TestCasillaBlanco.java
@@ -1,7 +1,38 @@
 package ar.edu.ub.buscaminas.casilla.testcase;
 
+import ar.edu.ub.buscaminas.casilla.CasillaBlanco;
+import ar.edu.ub.buscaminas.casilla.Coordenada;
+import ar.edu.ub.buscaminas.excepciones.CoordenadaIsNullException;
 import junit.framework.TestCase;
 
 public class TestCasillaBlanco extends TestCase {
+	
+	public void testCrearCasillaBlancoConPosicionNull() {
+		Coordenada coordenada = null;
+		boolean    pudoCrearCasilla = false;
+		
+		try {
+			new CasillaBlanco(coordenada);
+			pudoCrearCasilla = true;
+		} catch (CoordenadaIsNullException e) {
+			pudoCrearCasilla = false;
+		}
+		
+		assertFalse( pudoCrearCasilla );
+	}
+	
+	public void testCrearCasillaBlancoConPosicionNegativa() {
+		Coordenada coordenada = new Coordenada(-5, -5);
+		boolean pudoCrearCasilla = false;
+		
+		try {
+			new CasillaBlanco(coordenada);
+			pudoCrearCasilla = true;
+		}catch (Exception e) {
+			pudoCrearCasilla = false;
+		}
+		
+		assertFalse(pudoCrearCasilla);
+	}
 
 }
