c179f1fc13bc95cce30dd5c160272cd1fbf9570c
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-06-14 16:52:35 -0300

    Se empieza el testcase de casillaBloqueada

--- testcase/ar/edu/ub/buscaminas/casilla/testcase/TestCasillaBloqueada.java
+++ testcase/ar/edu/ub/buscaminas/casilla/testcase/TestCasillaBloqueada.java
@@ -1,7 +1,39 @@
 package ar.edu.ub.buscaminas.casilla.testcase;
 
+import ar.edu.ub.buscaminas.casilla.CasillaBloqueada;
+import ar.edu.ub.buscaminas.casilla.Coordenada;
+import ar.edu.ub.buscaminas.excepciones.CoordenadaIsNullException;
 import junit.framework.TestCase;
 
 public class TestCasillaBloqueada extends TestCase {
 
+	// crear casilla bloqueada con posicion null
+	
+	public void testCrearCasillaBloqueadaConPosicionNull() {
+		Coordenada coordenada = null;
+		boolean    pudoCrearCasilla = false;
+		
+		try {
+			new CasillaBloqueada( coordenada );
+			pudoCrearCasilla = true;
+		} catch (CoordenadaIsNullException e) {
+			pudoCrearCasilla = false;
+		}
+		
+		assertFalse( pudoCrearCasilla );
+	}
+	
+	public void testCrearCasillaBombaConPosicionNegativa() {
+		Coordenada coordenada = new Coordenada(-5, -5);
+		boolean pudoCrearCasilla = false;
+		
+		try {
+			new CasillaBloqueada( coordenada );
+			pudoCrearCasilla = true;
+		}catch (Exception e) {
+			pudoCrearCasilla = false;
+		}
+		
+		assertFalse(pudoCrearCasilla);
+	}
 }
