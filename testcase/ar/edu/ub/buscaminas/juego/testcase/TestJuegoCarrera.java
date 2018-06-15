9b3b4cf5c0b8bc08eb28af7b5901cab98d3bf324
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-06-15 17:22:24 -0300

    se modifica testcase

--- testcase/ar/edu/ub/buscaminas/juego/testcase/TestJuegoCarrera.java
+++ testcase/ar/edu/ub/buscaminas/juego/testcase/TestJuegoCarrera.java
@@ -12,7 +12,7 @@ import junit.framework.TestCase;
 
 public class TestJuegoCarrera extends TestCase {
 
-	public void testCrearJuegoCarrera() {
+	public void testCrearJuegoCarreraCorta() {
 		
 		
 		Jugador jugador = new Jugador("simba");
@@ -28,6 +28,39 @@ public class TestJuegoCarrera extends TestCase {
 
 
 		ITablero tablero = 	TableroCarrera.crearTableroPartidaCorta();	
+		
+		
+		boolean pudoCrearPartida = false;
+		
+		try {
+			new JuegoCarrera(tablero,jugadores);
+			pudoCrearPartida = true;
+		}catch(JuegoException e) {
+			pudoCrearPartida = false;
+		}
+		
+		assertTrue(pudoCrearPartida);
+	
+		
+		
+	}
+	
+	public void testCrearJuegoCarreraLarga() {
+		
+		
+		Jugador jugador = new Jugador("simba");
+		Jugador jugador2 = new Jugador("pumba");
+		Jugador jugador3 = new Jugador("simba");
+		Jugador jugador4 = new Jugador("simba");
+		
+		List<Jugador> jugadores = new ArrayList<Jugador>();
+		jugadores.add(jugador);
+		jugadores.add(jugador2);
+		jugadores.add(jugador3);
+		jugadores.add(jugador4);
+
+
+		ITablero tablero = 	TableroCarrera.crearTableroPartidaLarga();	
 		
 		
 		boolean pudoCrearPartida = false;
