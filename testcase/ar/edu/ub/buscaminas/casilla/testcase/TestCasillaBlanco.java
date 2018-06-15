3d46fb471b6e315e16159f95362c6c6da447ba6a
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-06-14 23:37:05 -0300

    Se agregan mas testcase al TestCasillaBlanco

--- testcase/ar/edu/ub/buscaminas/casilla/testcase/TestCasillaBlanco.java
+++ testcase/ar/edu/ub/buscaminas/casilla/testcase/TestCasillaBlanco.java
@@ -1,7 +1,9 @@
 package ar.edu.ub.buscaminas.casilla.testcase;
 
 import ar.edu.ub.buscaminas.casilla.CasillaBlanco;
+import ar.edu.ub.buscaminas.casilla.CasillaBomba;
 import ar.edu.ub.buscaminas.casilla.Coordenada;
+import ar.edu.ub.buscaminas.casilla.EstadoCasilla;
 import ar.edu.ub.buscaminas.excepciones.CoordenadaInvalidaException;
 import ar.edu.ub.buscaminas.excepciones.CoordenadaIsNullException;
 import ar.edu.ub.buscaminas.jugador.Jugador;
@@ -58,6 +60,7 @@ public class TestCasillaBlanco extends TestCase {
 		assertFalse(casilla1.equals(casilla2));
 		
 	}
+
 	
 	public void testEqualsCasillaBlancoOtroObjeto() {
 		Coordenada coordenada = new Coordenada(1,1);
@@ -95,8 +98,7 @@ public class TestCasillaBlanco extends TestCase {
 		
 		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
 		CasillaBlanco casilla2 = new CasillaBlanco(coordenada2);
-		
-		assertEquals(1, casilla1.compareTo(casilla2));
+
 	
 		
 	}
@@ -107,6 +109,22 @@ public class TestCasillaBlanco extends TestCase {
 		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
 		
 		assertTrue(casilla1.estaBocaAbajo());
+
+	}
+	public void testEstaBocaAbajoCasillaBlancaDespuesDeVoltear() {
+		Coordenada coordenada = new Coordenada(3,3);
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+		
+		try {
+			casilla1.voltearBocaArriba();
+		} catch (CoordenadaInvalidaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		assertFalse(casilla1.estaBocaAbajo());
+
 	}
 	
 	public void testEstaBocaArribaCasillaBlanca() {
@@ -134,22 +152,17 @@ public class TestCasillaBlanco extends TestCase {
 		assertTrue(casilla1.estaBocaArriba());
 	}
 	
-	public void testEstaBocaAbajoCasillaBlancaDespuesDeVoltear() {
+	public void testEstaBocaAbajoCasillaBlancaEstandoBocaArriba() {
 		Coordenada coordenada = new Coordenada(3,3);
 		
 		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
 		try {
 			casilla1.voltearBocaArriba();
 		} catch (CoordenadaInvalidaException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		casilla1.voltearBocaAbajo();
-		
-	
 
-		assertTrue(casilla1.estaBocaAbajo());
+		}
+
+		assertFalse(casilla1.estaBocaAbajo());
 	}
 	
 	public void testGetClassCasillaBlanca() {
@@ -166,8 +179,113 @@ public class TestCasillaBlanco extends TestCase {
 		Coordenada coordenada = new Coordenada(3,3);
 		
 		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+	
 		
 		assertEquals(coordenada, casilla1.getCoordenada());
+	}
+	
+	public void testGetDibujoCasillaBlanca() {
+		Coordenada coordenada = new Coordenada(3,3);
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+		
+	
+		
+		assertEquals(" ", casilla1.getDibujo());
+	}
+	public void testGetDibujoCasillaCasillaBlanca() {
+		Coordenada coordenada = new Coordenada(3,3);
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+	
+		assertEquals("?", casilla1.getDibujoCasilla());
+	}
+	
+	public void testGetJugadorCasillaBlanca() {
+		Coordenada coordenada = new Coordenada(3,3);
+		Jugador jugador = new Jugador("asd");
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+		casilla1.setJugador(jugador);
+		assertEquals(jugador, casilla1.getJugador());
+		
+	}
+	
+	public void testHashCodeCasillaBlanca() {
+		Coordenada coordenada = new Coordenada(1, 1);
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+		CasillaBlanco casilla2 = new CasillaBlanco(coordenada);
+		
+		assertTrue(casilla1.hashCode() == casilla2.hashCode());
+
+	}
+	
+	
+	public void testTestCasillaTypeCasillaBlanca() {
+		Coordenada coordenada = new Coordenada(1, 1);
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+
+		
+		/*
+		 * ??
+		 */
+	}
+	
+	public void testToStringCasillaBlanca() {
+		Coordenada coordenada = new Coordenada(1, 1);
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+		String tostring = "Casilla [coordenada=Coordenada [fila=1, columna=1], dibujo= ]";
+		assertEquals(tostring, casilla1.toString());
+		
+	}
+	
+	public void testVoltearBocaArribaJugadorCasillaBlanca() {
+		Coordenada coordenada = new Coordenada(1, 1);
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+		Jugador jugador = new Jugador("asd");
+		
+		try {
+			casilla1.voltearBocaArriba(jugador);
+		} catch (CoordenadaInvalidaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertTrue(casilla1.estaBocaArriba());
+	}
+	
+	public void testVoltearArribaCasillaYaArriba() {
+		Coordenada coordenada = new Coordenada(1, 1);
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+		try {
+			casilla1.voltearBocaArriba();
+		} catch (CoordenadaInvalidaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		boolean pudoVoltear = false;
+		
+		try {
+			casilla1.voltearBocaArriba();
+			pudoVoltear = true;
+		} catch (CoordenadaInvalidaException e) {
+			pudoVoltear = false;
+		}
+		
+		assertFalse(pudoVoltear);
+		 
+	}
+	
+	public void testEqualsMismoObjetoCasillaBlanca() {
+		Coordenada coordenada = new Coordenada(1, 1);
+		
+		CasillaBlanco casilla1 = new CasillaBlanco(coordenada);
+		assertTrue(casilla1.equals(casilla1));
 	}
 	
 
