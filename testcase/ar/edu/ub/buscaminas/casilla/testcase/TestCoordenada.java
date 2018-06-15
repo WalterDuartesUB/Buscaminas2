22358e110e7bab94f8c69c8509f0f4c28f2c2ce4
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-06-15 01:22:13 -0300

    se crea el testcase TestCoordenada

--- testcase/ar/edu/ub/buscaminas/casilla/testcase/TestCoordenada.java
+++ testcase/ar/edu/ub/buscaminas/casilla/testcase/TestCoordenada.java
@@ -1,6 +1,8 @@
 package ar.edu.ub.buscaminas.casilla.testcase;
 
+
 import ar.edu.ub.buscaminas.casilla.Coordenada;
+import ar.edu.ub.buscaminas.jugador.Jugador;
 import junit.framework.TestCase;
 
 public class TestCoordenada extends TestCase {
@@ -19,23 +21,26 @@ public class TestCoordenada extends TestCase {
 		assertFalse(pudoCrearCoordenada);
 	}
 	
-	public void testCrearCoordenadaNegativa() {
-		boolean pudoCrearCoordenada = false;
-		
-		try {
-			new Coordenada(-4, -4);
-			pudoCrearCoordenada = true;
-		}catch(Exception e ) {
-			pudoCrearCoordenada = false;
-		}
-		assertFalse(pudoCrearCoordenada);
-	}
 	
 	public void testCrearCoordenadaValoresPositivo() {
 		boolean pudoCrearCoordenada = false;
 		try {
 			new Coordenada(1,1);
 			pudoCrearCoordenada=true;
+		}catch(Exception e) {
+			pudoCrearCoordenada = false;
+		}
+		
+		assertTrue(pudoCrearCoordenada);
+	}
+	
+	public void testCrearCoordenadaConUnaCoordenada() {
+		Coordenada coordenada = new Coordenada(1,1);
+		boolean pudoCrearCoordenada = false;
+		
+		try {
+			new Coordenada(coordenada);
+			pudoCrearCoordenada = true;
 		}catch(Exception e) {
 			pudoCrearCoordenada = false;
 		}
@@ -56,5 +61,102 @@ public class TestCoordenada extends TestCase {
 		
 		assertEquals(fila, coordenada.getFila());
 
+	}
+	
+	public void testHasCodeCoordenada() {
+		Coordenada coordenada = new Coordenada(1,1);
+		Coordenada coordenada2 = new Coordenada(1,1);
+		
+		assertTrue(coordenada.hashCode() == coordenada2.hashCode());
+	}
+	
+	public void testEqualsCoordenadaMismaCasilla(){
+		Coordenada coordenada = new Coordenada(1,1);
+		Coordenada coordenada2 = new Coordenada(1,1);
+		
+		assertTrue(coordenada.equals(coordenada2));
+	}
+	
+	public void testEqualsCoordenadaDistintaCasilla(){
+		Coordenada coordenada = new Coordenada(1,1);
+		Coordenada coordenada2 = new Coordenada(1,1);
+		
+		assertTrue(coordenada.equals(coordenada2));
+		
+	}
+
+	
+	public void testEqualsCoordenadaOtroObjeto() {
+		Coordenada coordenada = new Coordenada(1,1);
+		Jugador jugador = new Jugador("asd");
+		
+		assertFalse(coordenada.equals(jugador));
+	}
+	
+	public void testEqualsMismoObjetoCoordenada() {
+		Coordenada coordenada = new Coordenada(1,1);
+		assertTrue(coordenada.equals(coordenada));
+	}
+	public void testEqualsObjetoNullCoordenada() {
+		Coordenada coordenada = new Coordenada(1,1);
+		Coordenada coordenada2 = null;
+		
+		assertFalse(coordenada.equals(coordenada2));
+	}
+	
+	public void testEqualsDistintaFilaCoordenada() {
+		Coordenada coordenada = new Coordenada(1,1);
+		Coordenada coordenada2 = new Coordenada(2,1);
+		
+		assertFalse(coordenada.equals(coordenada2));
+	}
+	public void testEqualsDistintaColumnaCoordenada() {
+		Coordenada coordenada = new Coordenada(1,6);
+		Coordenada coordenada2 = new Coordenada(1,1);
+		
+		assertFalse(coordenada.equals(coordenada2));
+	}
+	
+	public void testCompareCoordenadaMenor() {
+		Coordenada coordenada = new Coordenada(1,1);
+		Coordenada coordenada2 = new Coordenada(2,2);
+	
+		
+		assertEquals(-1, coordenada.compareTo(coordenada2));
+	}
+	
+	public void testCompareCoordenadaIgual() {
+		Coordenada coordenada = new Coordenada(2,2);
+		Coordenada coordenada2 = new Coordenada(2,2);
+
+		
+		assertEquals(0, coordenada.compareTo(coordenada2));
+		
+	}
+	
+	public void testCompareCoordenadaMayor() {
+		Coordenada coordenada = new Coordenada(3,3);
+		Coordenada coordenada2 = new Coordenada(2,2);
+
+		
+		assertEquals(1, coordenada.compareTo(coordenada2));
+
+	}
+	
+	public void testToStringCoordenada() {
+		Coordenada coordenada = new Coordenada(1, 1);
+
+		String tostring = "Coordenada [fila=1, columna=1]";
+		
+		assertEquals(tostring, coordenada.toString());
+		
+	}
+	
+	public void testSumarCoordenada() {
+		Coordenada coordenada = new Coordenada(1, 1);
+		Coordenada coordenada2 = new Coordenada(6, 6);
+		
+		assertEquals(coordenada2, coordenada.sumar(5, 5));
+		
 	}
 }
